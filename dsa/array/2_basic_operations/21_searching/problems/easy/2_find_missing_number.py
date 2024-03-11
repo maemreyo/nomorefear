fe28b13ac9461ac84656d19d021e6aeac1ec1bab
maemreyo <ngonhuthanhtrung1409@gmail.com>
2024-03-11 23:32:07 +0700

    Update 2_find_missing_number.py

---
+++ dsa/array/2_basic_operations/21_searching/problems/easy/2_find_missing_number.py
@@ -0,0 +1,27 @@
+"""
+    ❓ Question:
+        =>> Given an array arr[] of size n-1 with integers in the range [1, n], the tash is to find the missing number from the first n integers. Assume there are no duplicated in the list.
+
+    🚀 Expected Complexities:
+        ⌛ Time complexity: O(n)
+        🌌 Space complexity: O(1)
+
+    🗳️ Example:
+        Input: arr[] = [1, 2, 4, 6, 3, 7, 8]
+        Output: 5
+        Explanation: Here the size of the array is 7, so the range will be [1, 8]. The missing number between 1 to 8 is 5
+        
+        Input: arr[] = [1, 2, 3, 5]
+        Output: 4
+        Explanation: Here the size of the array is 4, so the range will be [1, 5]. The missing number between 1 to 5 is 4
+
+    🙌🏻 Approaches:
+        =>> Approach 1
+        
+        =>> Approach 2
+        
+        =>> Approach 3
+        
+
+====================================================================================================================================================================
+"""
