eee51a03e727f547fae580062b172ff5fb86e7cf
maemreyo <ngonhuthanhtrung1409@gmail.com>
2024-03-13 21:16:07 +0700

    Update 3_find_first_repeating.py
    Added an exercise

---
+++ dsa/array/2_basic_operations/21_searching/problems/easy/3_find_first_repeating.py
@@ -0,0 +1,23 @@
+"""
+    ❓ Question:
+        =>> Given an array of integers arr[], The task is to find the index of first repeating element in it i.e. the element that occurs more than once and whose index of the first occurrence is the smallest. 
+
+    🚀 Expected Complexities:
+        ⌛ Time complexity: O(n) or O(n^2)
+        🌌 Space complexity: O(1) or O(n)
+
+    🗳️ Example:
+        Input: arr[] = {10, 5, 3, 4, 3, 5, 6}
+        Output: 5 
+        Explanation: 5 is the first element that repeats
+
+        Input: arr[] = {6, 10, 5, 4, 9, 120, 4, 6, 10}
+        Output: 6 
+        Explanation: 6 is the first element that repeats
+
+    🙌🏻 Approach:
+        =>> Approach 1:
+        - 
+
+====================================================================================================================================================================
+"""
