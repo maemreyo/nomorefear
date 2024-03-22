497be6c6fb65944c1123c70d466ce9a649286682
matthew_ngo <ngonhuthanhtrung1409@gmail.com>
2024-03-22 21:17:52 +0700

    Added method to reverse an array in-place using RECURSION

--- dsa/array/2_basic_operations/22_reverse/algorithms/in-place/tests.py
+++ dsa/array/2_basic_operations/22_reverse/algorithms/in-place/tests.py
@@ -1,5 +1,6 @@
 import unittest
 from using_loop import reverse__loop
+from using_recursion import reverse__recursion
 
 
 class TestReverseInplace(unittest.TestCase):
@@ -7,6 +8,12 @@ class TestReverseInplace(unittest.TestCase):
         arr = [1, 2, 3, 4]
         expected = [4, 3, 2, 1]
         self.assertEqual(reverse__loop(arr), expected)
+
+    def test_reverse_using_recursion(self):
+        arr = [1, 2, 3, 4]
+        expected = [4, 3, 2, 1]
+        reverse__recursion(arr, 0, len(arr) - 1)
+        self.assertEqual(expected, arr)
 
 
 if __name__ == '__main__':
