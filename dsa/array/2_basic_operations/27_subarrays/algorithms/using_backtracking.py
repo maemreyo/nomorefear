0bf965772b5e9259e5f5bac9fb04ea6498b08f54
matthew_ngo <ngonhuthanhtrung1409@gmail.com>
2024-03-22 23:09:30 +0700

    Added an outline for learning Sub-arrays

---
+++ dsa/array/2_basic_operations/27_subarrays/algorithms/using_backtracking.py
@@ -0,0 +1,21 @@
+"""
+    ❓ Question:
+        =>> Given an array, generate all the possible sub-arrays of the given array using recursion.
+
+    🗳️ Example:
+        Input : [1, 2, 3]
+        Output : [1], [1, 2], [2], [1, 2, 3], [2, 3], [3]
+
+        Input : [1, 2]
+        Output : [1], [1, 2], [2]
+
+    🙌🏻 Approach:
+        ! Using BACKTRACKING
+        ->
+
+        🚀 Complexities:
+            ⌛ Time complexity: O()
+            🌌 Space complexity: O()
+
+"""
+# ! ==============================================================================================================
