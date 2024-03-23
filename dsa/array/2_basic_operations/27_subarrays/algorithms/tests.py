ebae49242c5eb9da31a2ae2952f37473618e7a36
matthew_ngo <ngonhuthanhtrung1409@gmail.com>
2024-03-23 08:40:15 +0700

    Added an advanced method using RECURSION to generate sub-arrays

--- dsa/array/2_basic_operations/27_subarrays/algorithms/tests.py
+++ dsa/array/2_basic_operations/27_subarrays/algorithms/tests.py
@@ -1,9 +1,10 @@
 import unittest
 from using_loop import find_sub_arrays__loop
+from using_recursion import find_sub_arrays__recursion
 
 
 class TestFindSubArrays(unittest.TestCase):
-    def test_finding_sub_arrays__loop_(self):
+    def test_finding_sub_arrays__loop_1(self):
         arr = [1, 2, 3]
         sub_arrays = [[1], [1, 2], [2], [1, 2, 3], [2, 3], [3]]
         self.assertCountEqual(sub_arrays, find_sub_arrays__loop(arr))
@@ -12,6 +13,16 @@ class TestFindSubArrays(unittest.TestCase):
         arr = [1, 2]
         sub_arrays = [[1], [1, 2], [2]]
         self.assertCountEqual(sub_arrays, find_sub_arrays__loop(arr))
+
+    def test_finding_sub_arrays__recursion_1(self):
+        arr = [1, 2, 3]
+        sub_arrays = [[1], [1, 2], [2], [1, 2, 3], [2, 3], [3]]
+        self.assertCountEqual(sub_arrays, find_sub_arrays__recursion(arr))
+
+    def test_finding_sub_arrays__recursion_2(self):
+        arr = [1, 2]
+        sub_arrays = [[1], [1, 2], [2]]
+        self.assertCountEqual(sub_arrays, find_sub_arrays__recursion(arr))
 
 
 if __name__ == '__main__':
