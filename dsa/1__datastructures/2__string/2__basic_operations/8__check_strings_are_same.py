2cce69d5d34f34e5f5c3f78cd7bfc7df90361b0e
matthew_ngo <ngonhuthanhtrung1409@gmail.com>
2024-03-26 00:03:18 +0700

    WIP

---
+++ dsa/1__datastructures/2__string/2__basic_operations/8__check_strings_are_same.py
@@ -0,0 +1,40 @@
+"""
+    |❓ PROBLEM
+        =>> Program to check if two strings are same or not
+        Given two strings, the task is to check if these two strings are identical(same) or not.
+
+
+
+    ! =================================================================================================================
+    |🗳️ EXAMPLE
+        Input: string1 = “GeeksforGeeks”, string2 = “GeeksforGeeks”
+        Output: Yes
+        Input: string1 = “Geeks for Geeks”, string2 = “Geeks for Geeks”
+        Output: Yes
+        Input: string1 = “GeeksforGeeks”, string2 = “Geeks”
+        Output: No
+        Input: string1 = “Geeks for Geeks”, string2 = “Geeks for geeks”
+        Output: No
+
+    ! =================================================================================================================
+    |🙌🏻 APPROACHES
+        @Approach 1:
+            ! Using BRUTE FORCE
+
+
+        |🚀 COMPLEXITIES:
+            ⌛ Time complexity: O()
+            🌌 Space complexity: O()
+
+        ! =============================================================================================================
+        @Approach 2:
+            ! Using
+
+
+        |🚀 COMPLEXITIES:
+            ⌛ Time complexity: O()
+            🌌 Space complexity: O()
+
+"""
+# ! ===================================================================================================================
+# ! Approach 1
