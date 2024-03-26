1f06d99a5f212fffc8701dd9d0de88d38350860a
maemreyo <ngonhuthanhtrung1409@gmail.com>
2024-03-26 14:53:12 +0700

    Added an easy approach to solve problem

---
+++ dsa/1__datastructures/2__string/2__basic_operations/10__remove_occurrences.py
@@ -0,0 +1,27 @@
+"""
+    |❓ PROBLEM
+        =>> Remove all occurrences of a character in a string
+        Given a string. Write a program to remove all the occurrences of a character in the string.
+
+    ! =================================================================================================================
+    |🗳️ EXAMPLE
+        Input : s = "geeksforgeeks"
+                c = 'e'
+        Output : s = "gksforgks"
+
+        Input : s = "geeksforgeeks"
+                c = 'g'
+        Output : s = "eeksforeeks"
+
+        Input : s = "geeksforgeeks"
+                c = 'k'
+        Output : s = "geesforgees"
+
+    ! =================================================================================================================
+    |🙌🏻 APPROACHES
+        @Approach 1:
+            ! Using BUILTIN METHOD
+            -> replace()
+
+"""
+# ! ===================================================================================================================
